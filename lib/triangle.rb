1a6bdf386b0e297c2131c8bc488d5f01c78e593f
Omar Nabi <onabi20@gmail.com>
2020-04-27 04:34:10 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,20 +1,33 @@
-class Triangle
-  attr_accessor :a, :b, :c 
   
-  def initialize (a, b, c)
+class Triangle
+  # write code here
+  attr_accessor :a, :b, :c
+  def initialize(a,b,c)
     @a = a 
     @b = b 
     @c = c 
   end
-  
+
   def kind
   a, b, c = [@a, @b, @c].sort
   raise TriangleError if a <= 0 or a + b <= c
   return :equilateral if a == c
   return :isosceles if a == b or b == c
   return :scalene
-  
-class TriangleError < StandardError
+    # if ((side1 == side2) && (side2 == side3) && (side == side1))
+    #   return :equilateral
+    # elsif ((side1 == side2) || (side2 == side2) || (b == c))
+    #   return :isosceles
+    # else
+    #   return :scalene
+    # end
   end
 
+  
+
+
+  class TriangleError < StandardError
+
+  end
 end
+© 2020 GitHub, Inc.
