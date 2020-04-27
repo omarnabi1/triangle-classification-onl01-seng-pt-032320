204ddce4059a7ac0d4964fcc1b4ae8bf1d5afbf3
Omar Nabi <onabi20@gmail.com>
2020-04-27 04:37:31 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,25 +1,42 @@
 class Triangle
-  # write code here
-  attr_accessor :a, :b, :c
-  def initialize(a,b,c)
-    @a = a 
-    @b = b 
-    @c = c 
+  # write code here\
+
+  def initialize(s1, s2, s3)
+
+  
+    if s1 >= (s2 + s3) || s2 >= (s1 + s3) || s3 >= (s1 + s2)
+      raise TriangleError
+    end
+
+    if s1 <= 0 || s2 <= 0 ||s3 <= 0
+      raise TriangleError
+    end
+
+    if s1 == nil || s2 == nil ||s3 == nil
+      raise TriangleError
+    end
+
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
   end
 
   def kind
-  a, b, c = [@a, @b, @c].sort
-  raise TriangleError if a <= 0 or a + b <= c
-  return :equilateral if a == c
-  return :isosceles if a == b or b == c
-  return :scalene
-    
+    if @s1 == @s2 && @s1 == @s3
+      return :equilateral
+    elsif @s1 == @s2 || @s2 == @s3 || @s3 == @s1
+      return :isosceles
+    else
+      return :scalene
+    end
   end
 
-  
+end
 
 
-  class TriangleError < StandardError
 
+class TriangleError < StandardError
+  def message
+    "This is not a valid triangle."
   end
 end
